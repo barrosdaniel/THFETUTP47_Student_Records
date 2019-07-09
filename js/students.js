2f3a1d2e314a5a82ee03689a48649d5532dbb1ce
Daniel Barros <barros.daniel@outlook.com>
2019-07-10 07:49:38 +0800

    feature: Add students array and data

--- js/students.js
+++ js/students.js
@@ -1,1 +1,34 @@
 // name, track, achievements, points
+var students = [{
+    name: 'Daniel Barros',
+    track: 'Front-end',
+    achievements: 66,
+    points: 9687
+  },
+  {
+    name: 'Cristiano Ronaldo',
+    track: 'Android',
+    achievements: 21,
+    points: 2569
+  },
+  {
+    name: 'Eric Cantona',
+    track: 'UX',
+    achievements: 32,
+    points: 4587
+  },
+  {
+    name: 'Paul Pogba',
+    track: 'UX',
+    achievements: 41,
+    points: 6214
+  },
+  {
+    name: 'Lionel Messi',
+    track: 'Full-stack',
+    achievements: 87,
+    points: 11256
+  }
+];
+
+console.log(students);
