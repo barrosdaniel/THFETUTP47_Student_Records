53e88f2caaa888dc19577dac53da549b502ceae4
Daniel Barros <barros.daniel@outlook.com>
2019-07-10 20:12:08 +0800

    feature: Add feature to allow for multiple records with the same name to be printed to the page

--- js/studentReportWithFilters.js
+++ js/studentReportWithFilters.js
@@ -2,7 +2,7 @@
 1.DONE Prompt for student name or quit to exit;
 2.DONE Prompt appears over and over again until quit is entered
 3.DONE If matches a student's name, print student details to page and end loop
-4. Show message if search does not find a student with the name provided
+4.DONE Show message if search does not find a student with the name provided
 5. Add feature to allow for all records with the same name to be printed to the page
 */
 
@@ -14,24 +14,32 @@ function print(message) {
   output.innerHTML = message;
 }
 
-do {
-  var searchEnded = false;
-  var studentFound = false;
-  var input = prompt('Search student records: type a name [Jody] to search or type "quit" to exit.').toLowerCase();
+function searchStudent() {
 
-  for (var i = 0; i < students.length; i++) {
-    if (input === students[i].name.toLowerCase()) {
-      message += '<h2>Student: ' + students[i].name + '</h2>';
-      message += '<p>Track: ' + students[i].track + '</p>';
-      message += '<p>Points: ' + students[i].points + '</p>';
-      message += '<p>Achievements: ' + students[i].achievements + '</p>';
-      print(message);
-      input = 'quit';
-      studentFound = true;
+  do {
+    var studentFound = false;
+    var input = prompt('Search student records: type a name [Jody] to search or type "quit" to exit.').toLowerCase();
+
+    for (var i = 0; i < students.length; i++) {
+      if (input === students[i].name.toLowerCase()) {
+        message += '<h2>Student: ' + students[i].name + '</h2>';
+        message += '<p>Track: ' + students[i].track + '</p>';
+        message += '<p>Points: ' + students[i].points + '</p>';
+        message += '<p>Achievements: ' + students[i].achievements + '</p>';
+        print(message);
+        studentFound = true;
+      }
     }
+
+    if (studentFound === false && input != 'quit' && input !== null) {
+      alert('No students found with this name.');
+      searchStudent();
+    }
+
+    input = 'quit';
   }
-  if (studentFound === false && input != 'quit' && input !== null) {
-    alert('No students found with this name.');
-  }
+  while (input !== 'quit' || input === null);
+
 }
-while (input !== 'quit' || input === null);
+
+searchStudent();
--- js/students.js
+++ js/students.js
@@ -18,13 +18,13 @@ var students = [{
     points: 4587
   },
   {
-    name: 'Pogba',
+    name: 'Ronaldo',
     track: 'UX',
     achievements: 41,
     points: 6214
   },
   {
-    name: 'Messi',
+    name: 'Pogba',
     track: 'Full-stack',
     achievements: 87,
     points: 11256
